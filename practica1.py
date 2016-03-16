cba6a4af53de881d0cd4f6a044150bad2877d756
rcruiz <rc.ruiz@alumnos.urjc.es>
2016-03-16 23:57:40 +0100

    subiendo

--- practica1.py
+++ practica1.py
@@ -4,17 +4,35 @@
 """
 Aplicacion web simple para acortar URLs
 Rosa Cristina Ruiz Rivas
-Alumna de SAT
 """
 
 import webapp
 import urlparse
+import csv
 
 
 class p1(webapp.webApp):
+    # Diccionario cuya clave es la urlCompleta y el valor es el num asignado
+    dicURLreal = {}
+    # La clave es el numero asignado y el valor es la url completa
+    dicNum = {}
+    # La clave es la url completa y el valor la url corta. Para el GET
+    dic = {}
+
+    def leerCSV(self):
+        try:
+            with open("URLcortas.csv", "r") as csvfile:
+                lurlCortas_csv = csv.reader(csvfile, delimiter=" ")
+                for row in lurlCortas_csv:  # Puntaje = numero de URLcorta
+                    self.dicURLreal[row[0]] = int(row[1])
+                    self.dicNum[int(row[1])] = row[0]
+                    self.dic[row[0]] = "http://localhost:1234/" + row[1]
+                    print " ".join(row)
+        except IOError:  # Para crear el fichero CSV la primera vez 
+            URLcortas = open("URLcortas.csv","w")
+            URLcortas.close()
 
     def parse(self, request):
-
         peticion = request.split(' ')[0]
         print "Recibido: " + peticion
         recurso = request.split(' ')[1]  # Cojo '/recurso'
@@ -33,25 +51,31 @@ class p1(webapp.webApp):
         return (peticion, recurso, cuerpo)
 
     def process(self, parsedRequest):
-
         peticion = parsedRequest[0]
         recurso = parsedRequest[1]
         cuerpo = parsedRequest[2]
         formulario = '<FORM action="" method="post"><p>'
         formulario += '<LABEL for="URL">URL: </LABEL>'
-        formulario += '<INPUT type="text" id="url"><BR>'
+        formulario += '<INPUT type="text" name="url"><BR>'
         formulario += '<INPUT type="submit" value="Send"></p></FORM>'
+
+        if self.dicNum == {} or self.dicURLreal == {}:
+            self.leerCSV()
 
         if (peticion == "GET"):
             if recurso == "/":
                 codigoHTTP = "200 OK"
-                cuerpoHtml = str(dic) + formulario + str(cuerpo)
+                cuerpoHtml = formulario + str(cuerpo) + str(self.dic)
             else:
-                recursoNum = int(recurso[1:])
-                if recursoNum in dicNum:
-                    url = dicNum[recursoNum]
-                    codigoHTTP = "303 SEE OTHER\r\nLocation: " + url
-                    cuerpoHtml = str(cuerpo)
+                if recurso[1:].isdigit():
+                    recursoNum = int(recurso[1:])
+                    if recursoNum in self.dicNum:
+                        url = self.dicNum[recursoNum]
+                        codigoHTTP = "303 SEE OTHER\r\nLocation: " + url
+                        cuerpoHtml = str(cuerpo)
+                    else:
+                        codigoHTTP = "404 Not Found"
+                        cuerpoHtml = "Recurso no disponible"
                 else:
                     codigoHTTP = "404 Not Found"
                     cuerpoHtml = "Recurso no disponible"
@@ -65,23 +89,25 @@ class p1(webapp.webApp):
                 if url[0:4] != "http":  # Incluye http:// si es necesario
                     url = "http://" + url
                 # Gestion de diccionarios con URL reales y acortadas
-                # if dicURLreal.has_key(url):
-                if url in dicURLreal:
-                    numSec = dicURLreal[url]
+                if url in self.dicURLreal:
+                    numSec = self.dicURLreal[url]
                     URLcorta = "http://localhost:1234/" + str(numSec)
-                    URLlarga = '<a href="' + url + '">' + url + '</a>'
+                    URLlarga = '<a href="' + url + '">' + url + '</a><br>'
                     codigoHTTP = "200 OK"
                     cuerpoHtml = '<p>' + URLlarga + '<a href="' + url + '">'
-                    cuerpoHtml += dic[url] + '</a></p>'
+                    cuerpoHtml += str(self.dic[url]) + '</a></p>'
                 else:  # Determina URL acortada
-                    numSec = len(dicURLreal)
-                    dicURLreal[url] = numSec
-                    dicNum[numSec] = url
-                    print dicURLreal
+                    numSec = len(self.dicURLreal)
+                    self.dicURLreal[url] = numSec
+                    self.dicNum[numSec] = url
+                    # LLamada a CSV para escribir
+                    with open("URLcortas.csv", "a") as csvfile:
+                        elementoCSV = csv.writer(csvfile, delimiter=" ")
+                        elementoCSV.writerow([url, numSec])           
                     URLcorta = "http://localhost:1234/" + str(numSec)
-                    dic[url] = URLcorta
+                    self.dic[url] = URLcorta
                     codigoHTTP = "200 OK"
-                    cuerpoHtml = '<p><a href="' + dicNum[numSec] + '">'
+                    cuerpoHtml = '<p><a href="' + self.dicNum[numSec] + '">'
                     cuerpoHtml += URLcorta + '</a></p>'
             else:
                 codigoHTTP = "404 Not Found"
@@ -96,10 +122,5 @@ class p1(webapp.webApp):
 
 
 if __name__ == "__main__":
-    # Diccionario cuya clave es la urlCompleta y el valor es el num asignado
-    dicURLreal = {}
-    # La clave es el numero asignado y el valor es la url completa
-    dicNum = {}
-    # La clave es la url completa y el valor la url corta. Para el GET
-    dic = {}
+
     testWebApp = p1("localhost", 1234)
